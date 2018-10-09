e14709f3dc2400c5713c7e5e9a87a5810b00ac77
hydrix9 <chadlbourke@yahoo.com>
2018-10-08 20:53:52 -1000

    more comments

--- MonsterSpawner.cs
+++ MonsterSpawner.cs
@@ -7,6 +7,7 @@ using UnityEngine.AI;
 
 public class MonsterSpawner : MonoBehaviour {
 
+    //struct that allows us to serialize a weighted spawn chance in the editor corresponding to the levels specified
     [Serializable]
     public struct SpawnChance {
         public int level;
@@ -27,8 +28,10 @@ public class MonsterSpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        //spawn our monster instantly or add to delegate
         if (Server.instance.active)
-            Spawn(default(listing));
+            Spawn(default(listing)); //onStartServer passes a delegate with the listing from MMClient as a parameter
         else
             Server.instance.onStartServer += Spawn;
 
@@ -36,19 +39,26 @@ public class MonsterSpawner : MonoBehaviour {
 
     }
 
+
     void Spawn(listing l)
     {
-        current = Server.instance.AddEntity(Server.instance.GetEmptyID(), monster.name) as Entity;
+        current = Server.instance.AddEntity(Server.instance.GetEmptyID(), monster.name) as Entity; //spawn monster from name on Server
+
+        //set pos and rotation of spawned monster
         current.transform.parent = transform;
         current.transform.position = transform.position;
 
-        current.level = RollLevel();
+        current.level = RollLevel(); //figure out level
     }
 
+
+    /// <summary>
+    /// rolls weights that result in possible level. EX: weights 10, 20, 10 would result in 25% level 1, 50% level 2, 25% level 3
+    /// </summary>
     public int RollLevel()
     {
         int totalWeight = spawnChances.Sum(entry => entry.weight); //get all weights
-        int roll = UnityEngine.Random.Range(1, totalWeight);
+        int roll = UnityEngine.Random.Range(1, totalWeight); //roll between 1 and total
         if (spawnChances.Length > 0)
             return spawnChances.OrderBy((entry) => entry.weight).FirstOrDefault(entry => entry.weight > roll).level; //order from greatest chance to smallest, then return level corresponding to first element that was greater than roll, indicating we rolled that
         else
@@ -66,6 +76,9 @@ public class MonsterSpawner : MonoBehaviour {
 
     public bool snapToValid = false;
 
+    /// <summary>
+    /// in the editor GUI this is called constantly. This function calls function SnapToValid when the bool snapToValid is pressed
+    /// </summary>
     void OnDrawGizmos()
     {
         if (!snapToValid) return;
@@ -77,16 +90,22 @@ public class MonsterSpawner : MonoBehaviour {
 
     }
 
-    Vector3 calcTreesOffset = new Vector3(0, 3f, 0);
+
+    Vector3 calcTreesOffset = new Vector3(0, 3f, 0); // how far away to stay to check for trees to prevent spawning in trees
+
+    /// <summary>
+    /// snap to a valid position on the current navmesh. Called from a button in the GUI. Also called from SpawnGenerator to snap to a valid position when it generates a spawn
+    /// </summary>
     public virtual void SnapToValid()
     {
         NavMeshHit hit;
         for (int i = 10; i > 0; i--)
-        {
+        { //try 10 times to find valid position with no trees
+
             if (NavMesh.SamplePosition(transform.position, out hit, 100f, NavMesh.AllAreas))
             {
                 if (!Physics.SphereCast(new Ray(hit.position + calcTreesOffset, Vector3.down), 1.5f, 1.5f, WorldFunctions.worldMask))
-                {
+                { //spherecast from a position above the grid on the worldmask to check for trees
                     transform.position = hit.position;
                     break;
                 }
